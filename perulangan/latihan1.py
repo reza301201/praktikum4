6f696163a6b436e555967b1d60b5107dd83925d4
Reza301201 <rzkurniawan21@gmail.com>
2022-11-08 20:54:25 +0700

    menjelaskan file

---
+++ perulangan/latihan1.py
@@ -0,0 +1,10 @@
+# melakukan perulangan dari 0 sampai 10
+untuk  saya  dalam  rentang ( 0 , 10 ):
+    # menampilkan variabel i , (" " * 3) Artinya menambahkan spasi tiga kali lalu end => yaitu memulai baris baru setelah nilai terakhir
+    print ( i , " "  *  3 , akhir = '' )
+
+    # melakukan perulangan dari 1 sampai 10
+    untuk  j  dalam  rentang ( 1 , 10 ):
+        # menampilkan output j + i , (" " * 3) Artinya menambahkan spasi tiga kali lalu end => memulai baris baru setelah nilai terakhir
+        cetak ( j  +  i , " "  *  3 , akhir = '' )
+    cetak ()
